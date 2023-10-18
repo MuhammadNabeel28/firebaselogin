3c551b4784048f8de01bcbac22e300b62b543c07
MuhammadNabeel28 <nabeelmansoori28@gmail.com>
2023-10-19 00:28:49 +0500

    source Commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_login/firebase_options.dart';
+import 'package:firebase_login/login.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(
+     MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'login',
+      routes: {
+        'login':(context) => const Login();
+      },
+    ),
+  );
+}
+
+
